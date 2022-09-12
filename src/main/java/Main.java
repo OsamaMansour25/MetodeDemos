084bd87628c008858cba51d346d4d77603b753f4
OsamaMansour25 <osam0289@stud.kea.dk>
2022-09-12 09:17:39 +0200

    tilføjet Person klasse med konstruktør

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person(1.88);
+        Person p2 = new Person();
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+
+    private double højde;
+    public Person(double højde) {
+        this.højde = højde;
+
+    }
+    public Person() {
+
+    }
+}
