09ed03552552c9f7fba47195c83ea68523ff42c0
OsamaMansour25 <osam0289@stud.kea.dk>
2022-09-12 10:10:39 +0200

    Tilføjet get og set metoder

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         Person p1 = new Person(1.89);
-        Person p2 = new Person();
+        System.out.println(p1.getHøjde());
+
+        p1.setHøjde(1.90);
+        System.out.println("Ny højde " + p1.getHøjde());
     }
+
 }
